e7176963cd3833e5a4a4d188ddf100195b8361a0
Pranjul <pranjulmaurya.23@gmail.com>
2022-06-15 07:54:00 +0530

    15.6

--- java/CF467A.java
+++ java/CF467A.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class CF467A {
+    public static void main(String args[]){
     
+    Scanner sc = new Scanner(System.in);
+        int count=0;
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++){
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            int cap = q-p;
+            if (cap>0){
+                count+=1;
+                continue;  }
+            else{
+                continue;  }
+        }
+    System.out.println(count);
+    sc.close();
+    
+    }    
 }
